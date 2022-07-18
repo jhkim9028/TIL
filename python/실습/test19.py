b8d7e059357625d2f7cb03a1b8ff72e8dbdcf511
jhkim9028 <jhkim9028@gmail.com>
2022-07-18 22:03:16 +0900

    실습19 수정

--- python/실습/test19.py
+++ python/실습/test19.py
@@ -1,16 +1,18 @@
 # 양의 정수 number가 주어질 때, 숫자의 길이를 구하시오. 
 # 양의 정수number를 문자열로 변경하는 것은 금지입니다. str() 사용 금지
 
-number = int(input())
-
-def digit_length(n):
-    ans = 0
-
-    while n:
-        n //= 10
-        ans += 1
-
-    return ans
+n = int(input())
 
 
-print(digit_length(number))
+ans = 0
+
+while n:
+    n //= 10
+    ans += 1
+
+print(ans)
+
+
+# while number != 0:
+#       number = number // 10
+#       cnt += 1
