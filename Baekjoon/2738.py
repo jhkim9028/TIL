d2e6db89c0804ee6afc7680ba14762784bbe16c6
jhkim9028 <jhkim9028@gmail.com>
2023-02-15 22:28:49 +0900

    백준:2738

---
+++ Baekjoon/2738.py
@@ -0,0 +1,12 @@
+N, M = map(int,input().split())
+
+a = [list(map(int, input().split())) for _ in range(N)]
+b = [list(map(int, input().split())) for _ in range(N)]
+
+
+for i in range(N):
+    for j in range(M):
+        a[i][j] += b[i][j]
+
+for i in range(N):
+    print(*a[i])
