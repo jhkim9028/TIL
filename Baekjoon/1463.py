d7e1ad47d3913b283f0e1fd1e6101d566ef03f8b
jhkim9028 <jhkim9028@gmail.com>
2023-02-18 22:57:43 +0900

    백준:1463

---
+++ Baekjoon/1463.py
@@ -0,0 +1,15 @@
+x = int(input())
+
+
+dp = [0] * (x+1)
+
+for i in range(2,x+1):
+    
+    dp[i] = dp[i-1] + 1
+    
+    if not i % 3:
+        dp[i] = min(dp[i],dp[i // 3]+1)
+    if not i % 2:
+        dp[i] = min(dp[i],dp[i // 2]+1)
+
+print(dp[x])
