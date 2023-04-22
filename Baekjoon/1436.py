3f575a1a65c82459b469580cf7c3838826a5d306
jhkim9028 <jhkim9028@gmail.com>
2023-04-22 09:35:46 +0900

    백준:1436

---
+++ Baekjoon/1436.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+cnt = 0
+start = 666
+while True:
+    if '666' in str(start):
+        cnt += 1
+        if cnt == N:
+            print(start)
+            break
+        start += 1
+    else:
+        start += 1
+
