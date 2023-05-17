ae34ed994155456625fdf59946a5d8d24dab0d70
jhkim9028 <jhkim9028@gmail.com>
2023-05-17 09:04:20 +0900

    백준:11659

--- Baekjoon/11659.py
+++ Baekjoon/11659.py
@@ -1,8 +1,21 @@
-N,M = map(int, input().split())
+import sys
+input = sys.stdin.readline
 
-num = list(map(int, input().split()))
+N, M = map(int, input().split())
 
-for _ in range(M):
-    a,b = map(int, input().split())
-    
-    print(sum(num[a-1:b]))
+numbers = list(map(int,input().split()))
+
+sumNumbers = [0 for _ in range(N)]
+
+sumNumbers[0] = numbers[0]
+for n in range(1,N):
+    sumNumbers[n] = sumNumbers[n-1] + numbers[n]
+
+for m in range(M):
+    a,b = map(int,input().split())
+    if a == 1:
+        print(sumNumbers[b-1])
+    elif a == b:
+        print(numbers[a-1])
+    else:
+        print(sumNumbers[b-1] - sumNumbers[a-2])
